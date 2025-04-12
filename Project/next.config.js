f574da0385ac9ad2d04530390783f9ecc5753e76
Harsh-Wardhan-404 <harshwardhansaindane@gmail.com>
2025-04-13 01:54:44 +0530

    profile route fix

--- Project/next.config.js
+++ Project/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200], // Limited device sizes
     imageSizes: [16, 32, 48, 64, 96], // Limited image sizes
     minimumCacheTTL: 86400, // Cache for 24 hours
-    domains: ['plus.unsplash.com', 'images.unsplash.com'],
+    domains: ['plus.unsplash.com', 'images.unsplash.com', 'lh3.googleusercontent.com'],
   },
   webpack: (config, { isServer }) => {
     // Optimize chunking
